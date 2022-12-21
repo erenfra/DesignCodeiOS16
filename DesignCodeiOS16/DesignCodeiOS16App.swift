460c821995ca88d6d7760c1efcc88ba0a11d0b51
Renato Oliveira Fraga <rofraga74@gmail.com>
2022-12-20 22:56:41 -0600

    Initial Commit

---
+++ DesignCodeiOS16/DesignCodeiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS16App.swift
+//  DesignCodeiOS16
+//
+//  Created by Renato Oliveira Fraga on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
